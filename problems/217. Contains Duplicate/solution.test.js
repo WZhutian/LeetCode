00791975a396fb03aa5bd4b6cbca38057bada7fb
WuZhutian <wzhutian@gmail.com>
2018-03-13 09:06:23 +0800

    217 done

---
+++ problems/217. Contains Duplicate/solution.test.js
@@ -0,0 +1,13 @@
+module.exports=[{
+    input: [[]],
+    output: false
+},{
+    input: [[0]],
+    output: false
+},{
+    input: [[0,1]],
+    output: false
+},{
+    input: [[0,0,1]],
+    output: true
+},]
